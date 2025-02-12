dd92d1beb45bfe88811fcd2b85b1b8db417a4c7d
Lena Castillo <lenac@uninorte.edu.co>
2025-02-12 09:52:41 -0500

    lena coursefocus

--- lib/ui/pages/flutter_course_page.dart
+++ lib/ui/pages/flutter_course_page.dart
@@ -6,25 +6,142 @@ class FlutterCoursePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      // you may remove this widget if you don't want to use it
-      child: Column(
-        children: <Widget>[
-          buildHeader(),
-          buildCourseFocus(),
-          buildModules(),
-          buildProjectList(),
-        ],
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              buildHeader(),
+              buildCourseFocus(),
+              Divider(
+                height: 80,
+              ),
+              const SizedBox(height: 20),
+              buildModules(),
+              const SizedBox(height: 20),
+            ],
+          ),
+        ),
       ),
-    ));
+    );
   }
 
   Widget buildHeader() {
-    return const Text("Flutter course");
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+      color: Colors.blue,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Flutter course",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                "2015-10",
+                style: TextStyle(fontSize: 14, color: Colors.white),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.grey.shade800,
+                      shape: BoxShape.rectangle,
+                      borderRadius: BorderRadius.circular(10)),
+                  padding: const EdgeInsets.all(8.0),
+                  child: const Icon(Icons.search, color: Colors.white),
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.grey.shade800,
+                      shape: BoxShape.rectangle,
+                      borderRadius: BorderRadius.circular(10)),
+                  padding: const EdgeInsets.all(8.0),
+                  child: const Icon(Icons.notifications, color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 
   Widget buildCourseFocus() {
-    return const Text("Focus on the course");
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Text(
+            "Course focus",
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+        ),
+        const SizedBox(height: 8),
+        Container(
+          margin: const EdgeInsets.symmetric(horizontal: 16.0),
+          height: 5,
+          child: Row(
+            children: [
+              Flexible(flex: 2, child: Container(color: Colors.blue)),
+              Flexible(flex: 2, child: Container(color: Colors.red)),
+              Flexible(flex: 2, child: Container(color: Colors.yellow)),
+              Flexible(flex: 1, child: Container(color: Colors.grey[200])),
+            ],
+          ),
+        ),
+        SizedBox(height: 10),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              Text(
+                "UI development",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 14,
+                ),
+              ),
+              Text("Architecture",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 14,
+                  )),
+              Text(
+                "Design thinking",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 14,
+                ),
+              ),
+              Text(
+                "Testing",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 14,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 
   Widget buildModules() {
